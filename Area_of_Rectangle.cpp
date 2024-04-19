8c57c158f1636d537715d695b6269703a19ce4fd
amrish05 <amrishyadav407@gmail.com>
2024-04-19 23:48:03 +0530

    Area of Rectangle

---
+++ Area_of_Rectangle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+    int l,b, result;
+    cin >> l >> b;
+
+    result = l*b;
+    cout << result;
+    
+    return 0;
+}
