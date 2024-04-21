9c248ec5ab73360fb39fa2717118744d54a4696e
amrish05 <amrishyadav407@gmail.com>
2024-04-21 15:11:44 +0530

    Fibonacci Series

---
+++ Fibonacci_Number.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+
+int fibonacciNumber(int n){
+    // Write your code here.
+    int prev1 = 1;
+    int prev2 = 1;
+    long current = 1;
+    const int m = 1000000007;
+    int i = 3;
+    
+    if(n<3)
+    return 1;
+    else{
+    while(i<=n){
+        prev2 = current%m;
+        current = prev1 + prev2;
+        prev1 = prev2;
+        i++;
+    }
+    return current%m;
+    }
+}
