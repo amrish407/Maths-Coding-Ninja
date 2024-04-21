0064c257215a7c08712a88a9ee56c9b8dd274671
amrish05 <amrishyadav407@gmail.com>
2024-04-21 20:54:16 +0530

    Done

--- Fibonacci_Number.cpp
+++ Fibonacci_Number.cpp
@@ -20,3 +20,29 @@ int fibonacciNumber(int n){
     return current%m;
     }
 }
+
+
+
+
+//Another way
+/*
+#include <bits/stdc++.h> 
+
+int fibonacciNumber(int n){
+    
+    const int m = 1000000007;
+    
+    int F[n];
+    F[1] = 1;
+    F[2] = 1;
+    
+    if(n<3)
+    return 1;
+    else{
+        for(int i=3;i<=n;i++){
+            F[i] = (F[i-1] + F[i-2 ])%m ; 
+        }
+    return F[n];
+    }
+}
+*/
